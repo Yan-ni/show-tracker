aa5540b54ace03b09751456e2d1f1f2c9688b203
Anis <anis01000001@gmail.com>
2022-03-22 23:31:52 +0100

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,7 @@ dotenv.config();
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // routes
 const routes = require("./app/routes");
